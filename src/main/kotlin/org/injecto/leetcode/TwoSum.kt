c9635615cd47e7156912a10fa6595676333bf1af
Vladimir Glushenkov <injecto.wg@gmail.com>
2020-05-03 13:33:45 +0400

    two sum: more effective solution

--- src/main/kotlin/org/injecto/leetcode/TwoSum.kt
+++ src/main/kotlin/org/injecto/leetcode/TwoSum.kt
@@ -6,24 +6,19 @@ class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         require(nums.size > 1)
 
-        val elements = nums.withIndex().groupBy({ it.value }) { it.index }
-        for ((v, idxs) in elements) {
+        val elements = HashMap<Int, MutableList<Int>>()
+
+        for ((i, v) in nums.withIndex()) {
             val addend = target - v
 
-            val idxs2 = elements[addend]
-            if (idxs2 != null) {
-                if (idxs === idxs2) {
-                    // duplicate
-                    if (idxs.size == 1)
-                        // same single value
-                        continue
-
-                    return intArrayOf(idxs[0], idxs[1])
-                }
-                return intArrayOf(idxs[0], idxs2[0])
+            val idxs = elements[addend]
+            if (idxs != null) {
+                return intArrayOf(idxs.first(), i)
             }
-        }
 
+            elements.computeIfAbsent(v) { arrayListOf() }
+                .add(i)
+        }
 
         throw IllegalArgumentException("Unsolvable")
     }
